65ccdbbdef5477dad82f4487ddf95b45f4ceba7d
Aniket Singh <anisingh12@publicisgroupe.net>
2023-01-02 12:09:07 +0530

    bruh moment

---
+++ src/main/java/com/sapient/model/Message.java
@@ -0,0 +1,21 @@
+package com.sapient.model;
+
+public class Message {
+	private String greeting;
+
+	public Message() {
+
+	}
+
+	public Message(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public String getGreeting() {
+		return this.greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+}
